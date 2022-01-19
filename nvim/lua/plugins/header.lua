e7c7176f405552d242749afe5e86df851900bc31
ImCylon <imcylonrs@gmail.com>
2022-01-19 13:27:02 -0300

    :fire: prune(nvim): remove old files

--- nvim/lua/plugins/header.lua
+++
@@ -1,9 +0,0 @@
---[[--
-File              : header.lua
-Author            : I'mCylon <imcylonrs@gmail.com>
-Date              : 13.07.2021
-Last Modified Date: 13.07.2021
-Last Modified By  : I'mCylon <imcylonrs@gmail.com>
---]]--
-vim.g.header_field_author = 'I\'mCylon'
-vim.g.header_field_author_email = 'imcylonrs@gmail.com'
--- nvim/lua/plugins/hop.lua
+++
@@ -1,9 +0,0 @@
---[[--
-File              : hop.lua
-Author            : I'mCylon <imcylonrs@gmail.com>
-Date              : 10.11.2021
-Last Modified Date: 10.11.2021
-Last Modified By  : I'mCylon <imcylonrs@gmail.com>
---]]--
-
-require'hop'.setup()
