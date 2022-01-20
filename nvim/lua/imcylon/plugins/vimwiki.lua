609dcaf46436103e2f4da33e7c6e77e145be2c0a
ImCylon <imcylonrs@gmail.com>
2022-01-20 15:34:26 -0300

    :sparkles: feature(nvim): add vimwiki

---
+++ nvim/lua/imcylon/plugins/vimwiki.lua
@@ -0,0 +1,19 @@
+--[[--
+File              : vimwiki.lua
+Author            : I'mCylon <imcylonrs@gmail.com>
+Date              : 20.01.2022
+Last Modified Date: 20.01.2022
+Last Modified By  : I'mCylon <imcylonrs@gmail.com>
+--]]
+
+vim.g.vimwiki_list = {
+	{
+		path = "/home/imcylon/Documents/vimwiki/",
+		syntax = "markdown",
+		ext = ".md",
+	},
+}
+vim.g.vimwiki_global_ext = 0
+vim.g.vimwiki_key_mappings = {
+	all_maps = 0,
+}
