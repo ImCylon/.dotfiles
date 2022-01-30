854e7e2d0242ed67c51fdf7a7706f14a6973f0b8
ImCylon <imcylonrs@gmail.com>
2022-01-30 12:42:21 -0300

    :wrench: chore(nvim): sort init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,42 +2,41 @@
 File              : init.lua
 Author            : I'mCylon <imcylonrs@gmail.com>
 Date              : 14.01.2022
-Last Modified Date: 29.01.2022
+Last Modified Date: 30.01.2022
 Last Modified By  : I'mCylon <imcylonrs@gmail.com>
 --]]
 
+require("imcylon.abrev")
 require("imcylon.mappings")
 require("imcylon.options")
 require("imcylon.packs")
-require("imcylon.abrev")
 
 -- Settins plugins
+require("imcylon.plugins.alpha")
 require("imcylon.plugins.appearance")
 require("imcylon.plugins.autopairs")
+require("imcylon.plugins.blankline")
 require("imcylon.plugins.bufferline")
 require("imcylon.plugins.cmp")
+require("imcylon.plugins.colorizer")
 require("imcylon.plugins.comment")
+require("imcylon.plugins.floaterm")
 require("imcylon.plugins.gitsigns")
+require("imcylon.plugins.header")
+require("imcylon.plugins.hop")
+require("imcylon.plugins.illuminate")
+require("imcylon.plugins.impatient")
+require("imcylon.plugins.julia")
 require("imcylon.plugins.lsp")
+require("imcylon.plugins.lsp.null-ls")
+require("imcylon.plugins.lualine")
 require("imcylon.plugins.nvim-tree")
+require("imcylon.plugins.range-highlight")
+require("imcylon.plugins.slime")
 require("imcylon.plugins.tabnine")
 require("imcylon.plugins.telescope")
 require("imcylon.plugins.treesitter")
-require("imcylon.plugins.lsp.null-ls")
-require("imcylon.plugins.floaterm")
-require("imcylon.plugins.alpha")
-require("imcylon.plugins.impatient")
-require("imcylon.plugins.header")
-require("imcylon.plugins.lualine")
-require("imcylon.plugins.colorizer")
 require("imcylon.plugins.vim-browser-search")
-require("imcylon.plugins.blankline")
-require("imcylon.plugins.range-highlight")
-require("imcylon.plugins.illuminate")
-require("imcylon.plugins.hop")
-require("imcylon.plugins.julia")
-require("imcylon.plugins.slime")
-require("imcylon.plugins.vimwiki")
 require("imcylon.plugins.vimwiki")
 
 -- centralizer scroolloff
