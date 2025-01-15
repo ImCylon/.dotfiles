43621497a872b6c78fb1926936854bb51d0f89c7
I'mCylon <imcylonrs@gmail.com>
2025-01-14 22:16:10 -0300

    chore(dotfiles): change repository waybar to subtree

---
+++ crontab/slack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# File              : slack.sh
+# Author            : I'mCylon <imcylonrs@gmail.com>
+# Date              : 16.01.2023
+# Last Modified Date: 16.01.2023
+# Last Modified By  : I'mCylon <imcylonrs@gmail.com>
+/usr/bin/telegram-desktop
