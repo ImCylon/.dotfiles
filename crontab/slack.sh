c1817ed55ecb4008d0f7778fb7d0344d4aab6b1f
I'mCylon <imcylonrs@gmail.com>
2025-08-17 17:20:49 -0300

    feat: add configs to submodules
    BREAKING CHANGE: Remove olds configs

--- crontab/slack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# File              : slack.sh
-# Author            : I'mCylon <imcylonrs@gmail.com>
-# Date              : 16.01.2023
-# Last Modified Date: 16.01.2023
-# Last Modified By  : I'mCylon <imcylonrs@gmail.com>
-/usr/bin/telegram-desktop
